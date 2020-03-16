02bb9cccdd62bb849c1464bd5c1ca0ac7fa830ed
dch <dch@rjh.com.cn>
2020-03-16 16:25:18 +0800

    适配linux

--- CoreServer.Common/Configuration/AppConfig.cs
+++ CoreServer.Common/Configuration/AppConfig.cs
@@ -1,8 +1,28 @@
-﻿namespace CoreServer.Common.Configuration
+﻿using CoreServer.Common.Configuration.ConfigModel;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreServer.Common.Configuration
 {
     public static class AppConfig
     {
-        public static string ConnectionString { get; }
+        private static string _connectionString;
+
+        public static string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_connectionString))
+                {
+                    var databases = Configuration.GetSection("Databases")
+                        .Get<Database[]>();
+
+                    _connectionString = databases[0].ConnectionString;
+                }
+                
+                return _connectionString;
+            }
+        }
+
+        public static IConfiguration Configuration { private get; set; }
     }
-    
 }
